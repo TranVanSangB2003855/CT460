a1cd0f76a99cefedeff529643bfd9a646515f6c3
Tran Van Sang <114197833+TranVanSangB2003855@users.noreply.github.com>
2023-10-09 10:32:52 +0700

    init

---
+++ CoursesWebApp/src/main/java/com/example/courseswebapp/CoursesWebAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.courseswebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class CoursesWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesWebAppApplication.class, args);
+	}
+
+}
